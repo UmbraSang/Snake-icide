b50a2833800a0554e13b0d638a4f6c38a1c50d11
Jocelyn Yale <jocelyneyale@gmail.com>
2018-03-03 14:37:45 -0800

    movement psuedocode
    YO!

---
+++ snake.java
@@ -0,0 +1,35 @@
+public class snake{
+
+	public static void main{
+		numOfMoves = findMoves(current cell);
+		optimizeMoves(numOfMoves);
+	}
+	public static int findMoves(array of moves){
+		
+		check cell (x,y+1) (UP)
+			if((x,y+1)=0) inc # in correct index
+			if((x,y+1)=-1) correct index +=2
+		check cell (x,y-1) (down)
+			if((x,y-1)=0) inc # in correct index
+			if((x,y-1)=-1) correct index +=2
+		check cell (x+1,y) (right)
+			if((x+1,y)=0) inc # in correct index
+			if((x+1,y)=-1) correct index +=2
+		check cell (x-1,y) (left)
+			if((x-1,y)=0) inc # in correct index
+			if((x-1,y)=-1) correct index +=2
+		
+		return numOfWords;
+		
+	}
+	public static moveItOrLoseIt(numOfWords){  (returns the cell)
+		// we have an array of cells and array of the optimization num with matching indexes
+		int moveIndex;
+		for(int i=0; i<4;i++){
+			save largest number in array
+			moveIndex= 
+		}
+		move
+		
+	}
+}
